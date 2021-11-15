30121326b40911099f85d8b52f74d0f4ee47317c
thelegend30 <luibichenko.oleg@gmail.com>
2021-11-16 00:15:22 +0200

    New mega project has been started

---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static int result;
+
+    public static void main(String[] args) {
+        String h = "Oh,hi,Mark ";
+        System.out.println(h);
+    }
+}
+
